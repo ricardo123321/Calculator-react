56c2ab6dddf4f722669dddacbef5dc3b395706c4
ricardo <ricardotvera.54@gmail.com>
2021-01-04 10:49:24 -0800

    created component and deleted some files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -10,4 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-reportWebVitals();
