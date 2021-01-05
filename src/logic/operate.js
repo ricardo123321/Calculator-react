9c17276615a39b4d9cd1f4416ebbefb8fa597424
ricardo <ricardotvera.54@gmail.com>
2021-01-05 00:11:48 -0800

    Update operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,6 +3,18 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   const firstNumber = new Big(numberOne);
   const secondNumber = new Big(numberTwo);
+  switch (operation) {
+    case '+':
+      return one.plus(two).toString();
+    case '-':
+      return one.minus(two).toString();
+    case 'X':
+      return one.times(two).toString();
+    case '÷':
+      return one.div(two).toString();
+    default:
+      return '0';
+  }
 }
 
 export default operate;
