f823dc3ad45431bb066e9a72f403a76e65794b50
ricardo <ricardotvera.54@gmail.com>
2021-01-04 17:53:01 -0800

    Create operate.js

---
+++ src/logic/operate.js
@@ -0,0 +1,8 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const firstNumber = new Big(numberOne);
+  const secondNumber = new Big(numberTwo);
+}
+
+export default operate;
