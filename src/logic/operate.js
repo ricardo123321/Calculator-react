49f56ac2433dfc1fa97b427d0a7b8780fd0798b1
ricardo <ricardotvera.54@gmail.com>
2021-01-05 13:12:17 -0800

    fixed division by zero problem and added % calculation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,17 +3,24 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operation) => {
   const firstNumber = new Big(numberOne);
   const secondNumber = new Big(numberTwo);
+
   switch (operation) {
     case '+':
       return firstNumber.plus(secondNumber).toNumber();
+    case '÷':
+      try {
+        return firstNumber.div(secondNumber).toNumber();
+      } catch (e) {
+        return 'ERROR';
+      }
     case '-':
       return firstNumber.minus(secondNumber).toNumber();
     case 'X':
       return firstNumber.times(secondNumber).toNumber();
-    case '÷':
-      return firstNumber.div(secondNumber).toNumber();
+    case '%':
+      return firstNumber.times(Big(0.01)).toNumber();
     default:
-      return '0';
+      return -1;
   }
 };
 
