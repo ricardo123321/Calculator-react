08a2aea1baba835f1f581d659bce6582b583a40e
ricardo <ricardotvera.54@gmail.com>
2021-01-05 10:47:36 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ class App extends Component {
             `
             ${total || ''}
             ${operation || ''}
-             ${calc}`
+             ${next || ''}`
           }
         />
         <ButtonPanel clickHandler={this.handleClick} />
