58a44b969442ca358b8375120319511e3aaaa4fc
ricardo <ricardotvera.54@gmail.com>
2021-01-05 07:58:29 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,29 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+  }
+
+  render() {
+    const { next, total, operation } = this.state;
+
+    return (
+      <>
+        <Display/>
+        <ButtonPanel/>
+      </>
+    );
+  }
+
+}
 
 export default App;
